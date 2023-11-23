ea3a3713597bfa874e4f5c37eb955e828cff28ca
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-23 17:22:36 +0600

    add-api

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,9 +3,9 @@ import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
-router.post('/POST/api/users', UserControllers.createUser);
-router.get('/GET/api/users', UserControllers.getAllUser);
-router.get('/GET/api/users/:userId', UserControllers.getOneUser);
-router.delete('/DELETE/api/users/:userId', UserControllers.deleteOneUser);
+router.post('/api/users', UserControllers.createUser);
+router.get('/api/users', UserControllers.getAllUser);
+router.get('/api/users/:userId', UserControllers.getOneUser);
+router.delete('/api/users/:userId', UserControllers.deleteOneUser);
 
 export const UserRoute = router;
